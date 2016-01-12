776336b387e29a434df0d3eb8a7c682554def2cb
Lyovkin <anatoliymolinari@gmail.com>
2016-01-12 20:52:29 +0200

    add table students

--- database/migrations/2016_01_12_182948_create_students_table.php
+++ database/migrations/2016_01_12_182948_create_students_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateStudentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +12,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('students', function(Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('gender');
+            $table->string('group', 5);
             $table->string('email')->unique();
+            $table->integer('marks');
+            $table->date('birthday');
+            $table->string('phone', 15);
             $table->string('password', 60);
             $table->rememberToken();
             $table->timestamps();
@@ -29,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('students');
     }
 }
